74036fbf7b9f68bfc3f9a33842cb86e007a6e42e
rkota(Rosaire Kota) <rosaire@kinshasadigital.com>
2023-05-18 16:15:57 +0100

    refactor: update example file

--- examples/index.php
+++ examples/index.php
@@ -8,8 +8,10 @@ use RosaireKota\Convertor\ConvertNumberToLetters;
 
 $frConvertor = new ConvertNumberToLetters(); // the default language is fr
 
-// exemple 1: 2000
+//e.g.1: 2000
 echo $frConvertor->convertNumberToLetters(2000);
 
-// exemple 2: 2050
+//e.g.2 2050
 echo $frConvertor->convertNumberToLetters(2050);
+
+// Deux Milles  Cinquante
